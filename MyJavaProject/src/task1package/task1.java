64cd4eaec8efe1e6ac968e1f3a4ec93ca5a6ab31
Manail <manahilghouri100@gmail.com>
2025-06-16 16:05:58 +0500

    changes in code-pushing task

--- MyJavaProject/src/task1package/task1.java
+++ MyJavaProject/src/task1package/task1.java
@@ -3,6 +3,9 @@ package task1package;
 public class task1 {
 	  public static void main(String[] args) {
 	        System.out.println("Hello from GitHub!");
+	        System.out.println("Push this on git hub");
+
+	        
 	    }
 	}
 
